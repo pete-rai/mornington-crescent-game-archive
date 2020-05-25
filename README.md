b9814a73c3657b260db0f406544a7323984dcfc9
Pete Rai <pete@rai.org.uk>
2020-05-25 23:27:12 +0100

    First pass commit of archive

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
-# mornington-crescent-game-archive
-The Wisden of Mornington Crescent
+# Mornington Crescent Game Archive
+
+> Visit my [Blog](http://www.rai.org.uk) to get in touch or to see demos of my other work.
+
+## Overview
+
+Welcome to this archive! _You've found the Wisden of Mornington Crescent._
+
+Here you will find a detailed record of _every single game of Mornington Crescent that has ever been played_, since its inception in late August 1978 to the start of 2020. The archive scope is all games played in tournaments, leagues, clubs and societies. This is pretty much the whole shebang for the game history - barring personal games, pub games and things like random games played on [radio shows](https://en.wikipedia.org/wiki/I%27m_Sorry_I_Haven%27t_a_Clue) and such.
+
+> If you don't care about all the details and just want to explore the data, then you can use the [Game Explorer](http://rai.org.uk/mcga/) site for that.
+
+## The Opportunity
+
+This is a huge opportunity for any machine learning experts. I know that, in the past, ML researchers have balked at the complexity of taking on Mornington Crescent and have instead taken on simpler games such as [Chess](https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)) and [Go](https://en.wikipedia.org/wiki/AlphaGo). But now the data exists in one place and in one format, I hope that teams will step-up and try and build the first ever, credible robot player of this _very human_ game.
+
+## The Corpus
+
+Gathering this collection has been a labour of love for more than a decade. The records were quite literally 'all over the place'. When I first started collecting games, little did I know the years and miles that would be needed to complete the canonical set. I have been to every county in the United Kingdom and to a great many countries around the Earth.
+
+A key achievement was that I was granted (after a great deal of wrangling) access to the Knoeppfler archives. My sincere thanks to Dr Knoeppfler's family for allowing me to consult this tremendous resource. The Knoeppfler records were hand-written in tiny German script and I was not allowed (for obvious reasons) to remove the paper records from the estate - so this was a huge and time consuming challenge.
+
+I have included all the major game playing organisations worldwide. An odd-ball being the Indian Leagues, which disbanded in late 1998 (after fallout from the Fairlop schism). However, luckily in 2004 records of these were found in a locked cabinet in a basement of Mumbai's [Chunabhatti station](https://en.wikipedia.org/wiki/Chunabhatti_railway_station).
+
+Historians of the game will know, that we finally came to a conclusion on the mythical games which were played behind the [Iron Curtain](https://en.wikipedia.org/wiki/Iron_Curtain) in the late 1980s and early 90s. Word of these only emerged after the [fall of the Soviet Union](https://en.wikipedia.org/wiki/Dissolution_of_the_Soviet_Union), but for many years they were just a rumour. Then in 2012, Austrian historian Prof. Günther von Habsburg uncovered a partial record of games in the undestroyed records the [Stasi](https://en.wikipedia.org/wiki/Stasi). He finally discovered the complete set two years later. I can confirmed that one of these games, played in March 1988, has the _only recorded case of a triple-loop vector across diagonals made in open play_.
+
+I have _not_ included games played under 'Finchley Central' rules. This should not be a surprise, as this is _not_ Mornington Crescent - it is a totally different game! The 'Finchley Central' players are a strange bunch indeed, but as true aficionados of the game know, FC is not cannon. Also, please don't get me started on The Hornchurch Convention weirdos. I will leave it to our American cousins to digitise the Grand Central games.
+
+## The Data
+
+All the data is in the one file [games.json]() and, as the name suggests, is held in JSON format. It is giant array consisting of over 18,000 items. Each item is a single game in the following format:
+
+```json
+{
+    "id": "8958f7e9dcb793b178b62de5",
+    "started": "1978-09-13",
+    "duration": 161,
+    "players": 3,
+    "winner": 1,
+    "tags": [
+        "Interchanges can be reclaimed",
+        "Pivoting after opening play only",
+        "Peeking inverted"
+    ],
+    "moves": [
+        {
+            "player": 1,
+            "move": "Bromley-by-Bow",
+            "note": ""
+        },
+        {
+            "player": 2,
+            "move": "Brent Cross",
+            "note": "challenge | rejected | Peeking inverted"
+        },
+        {
+            "player": 3,
+            "move": "Uxbridge",
+            "note": "notwithstanding Pivoting after opening play only"
+        },
+        {
+            "player": 1,
+            "move": "Richmond",
+            "note": ""
+        },
+        {
+            "player": 2,
+            "move": "Earls Court",
+            "note": "tournament play"
+        },
+        {
+            "player": 3,
+            "move": "Whitechapel",
+            "note": "last gambit denied"
+        },
+        {
+            "player": 1,
+            "move": "Blackhorse Road",
+            "note": ""
+        },
+        {
+            "player": 2,
+            "move": "Cannon Street",
+            "note": "challenge | rejected | Interchanges can be reclaimed"
+        },
+        {
+            "player": 3,
+            "move": "Snaresbrook",
+            "note": ""
+        },
+        {
+            "player": 1,
+            "move": "Mornington Crescent",
+            "note": ""
+        }
+    ]
+}
+```
+
+Note that:
+
+* ```id``` is always a 28 character string
+* ```started``` dates are always in the format ```YYYY-MM-DD```
+* ```duration``` is always measured in seconds
+
+Happy Mornington Crescent playing and watch-this-space for the upcoming _Wobbling Bunnies_ game archive.
+
+_– [Pete Rai](http://www.rai.org.uk)_
---
+++ index.php
@@ -0,0 +1,309 @@
+<?php
+
+  define ('GAMES'     , 18881);
+  define ('GAMES_FILE', 'games.json');
+  define ('LINES_FILE', 'lines.csv');
+
+  // --- games file is too large to load all in one go - hence we stream read it
+
+  $game   = '';
+  $found  = 0;
+  $chosen = rand (1, GAMES);
+  $games  = fopen (GAMES_FILE, 'r');
+
+  while (!feof ($games))
+  {
+      $line = fgets ($games);
+
+      if (preg_match ("/^{/", $line))
+      {
+          $found++;
+      }
+
+      if ($chosen == $found)
+      {
+          $game .= trim ($line);
+
+          if (preg_match ("/^},/", $line))
+          {
+              break;
+          }
+      }
+  }
+
+  fclose ($games);
+
+  $game     = json_decode (rtrim ($game, ','));
+  $stations = array_map ('str_getcsv', file (LINES_FILE));
+  $colours  = [];
+
+  foreach ($stations as $station)
+  {
+      $colours [trim (array_shift ($station))] = strtolower (str_replace (' & ', '-', trim (array_shift ($station))));
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+  <title>Game #<?= $game->id ?> &raquo; Mornington Crescent</title>
+  <link href="https://fonts.googleapis.com/css?family=DM+Mono|Lato|Roboto&display=swap" rel="stylesheet" />
+  <style>
+
+    html, body
+    {
+        background: #e5e5e5;
+        font-family: 'Roboto', sans-serif;
+        padding: 10px;
+        overflow-x: hidden;
+    }
+
+    .container
+    {
+        margin: 0 auto;
+        max-width: 600px;
+        text-align: center;
+        width: 100%;
+    }
+
+    h1
+    {
+        font-family: 'Lato', sans-serif;
+        font-size: 1.5em;
+        font-weight: 200;
+        margin-top: 25px;
+    }
+
+    h2
+    {
+        font-size: 2.5em;
+        margin: 10px;
+    }
+
+    h3
+    {
+        color: #333;
+        font-size: 1.2em;
+        margin: 10px;
+    }
+
+    p
+    {
+        margin: 8px;
+    }
+
+    .info p:last-of-type
+    {
+        margin-top: 16px;
+        margin-bottom: 18px;
+    }
+
+    .tag
+    {
+        background: grey;
+        border-radius: 10px;
+        color: white;
+        display: inline-block;
+        margin-top: 10px;
+        margin-left: 6px;
+        padding: 6px 12px;
+    }
+
+    hr
+    {
+        border: 1px dashed grey;
+        margin-bottom: 20px;
+    }
+
+    .move
+    {
+        border: 1px solid #555;
+        border-radius: 8px;
+        box-shadow: 0px 3px 15px rgba(0,0,0,0.25);
+        box-sizing: border-box;
+        color:white;
+        display: flex;
+        margin-bottom: 20px;
+        padding: 10px;
+    }
+
+    .circle
+    {
+        background: white;
+        border: 1px solid grey;
+        border-radius: 50%;
+        color: black;
+        font-size: 1.1em;
+        font-weight: bold;
+        height: 28px;
+        text-align: center;
+        line-height: 28px;
+        vertical-align: middle;
+        width: 28px;
+    }
+
+    .player
+    {
+        flex: 7%;
+    }
+
+    .play
+    {
+        flex: 93%;
+        text-align: left;
+        padding-left: 10px;
+    }
+
+    .place
+    {
+        font-size: 1.5em;
+    }
+
+    .note
+    {
+        font-size: 1em;
+        font-style: italic;
+        margin-top: 4px;
+    }
+
+    .muted
+    {
+        color: grey;
+    }
+
+    .monospace
+    {
+        font-family: 'DM Mono', monospace;
+    }
+
+    pre.raw
+    {
+        background: black;
+        border-radius: 8px;
+        box-shadow: 0px 3px 15px rgba(0,0,0,0.25);
+        color: lightgreen;
+        font-family: 'DM Mono', monospace;
+        font-size: 1rem;
+        line-height: 1.3rem;
+        padding: 15px;
+        text-align: left;
+    }
+
+    a,
+    a:hover,
+    a:visited,
+    a:active
+    {
+        color:inherit;
+    }
+
+    .line-bakerloo
+    {
+        background: #b26300;
+    }
+
+    .line-central
+    {
+        background: #dc241f;
+    }
+
+    .line-circle
+    {
+        background: #ffd329;
+    }
+
+    .line-district
+    {
+        background: #007d32;
+    }
+
+    .line-dlr
+    {
+        background: #00b2a9;
+    }
+
+    .line-hammersmith-city
+    {
+        background: #f4a9be;
+    }
+
+    .line-jubilee
+    {
+        background: #a1a5a7;
+    }
+
+    .line-metropolitan
+    {
+        background: #9b0058;
+    }
+
+    .line-northern
+    {
+        background: #000000;
+    }
+
+    .line-piccadilly
+    {
+        background: #0019a8;
+    }
+
+    .line-victoria
+    {
+        background: #0098d8
+    }
+
+    .line-waterloo-city
+    {
+        background: #93ceba
+    }
+
+    .line-winner
+    {
+        background: #eee;
+        color: #de2110;
+        border: 1px solid #de2110;
+    }
+
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h2>Mornington Crescent</h2>
+    <h3>
+      <a href="#">Historical Game Archive</a>
+    </h3>
+    <div class="info">
+      <h1>GAMEID # <?= $game->id ?></h1>
+      <p>
+        <span class="muted">played on </span><?= date ('D jS M Y', strtotime ($game->started)) ?><span class="muted"> for </span><?= ceil ($game->duration / 60); ?><span class="muted"> mins</span>
+      </p>
+      <p>
+        <span class="muted">between </span><?= $game->players ?><span class="muted"> players</span>
+      </p>
+      <?php foreach ($game->tags as $tag) { ?>
+        <span class="tag"><?= $tag ?></span>
+      <?php } ?>
+      <p class="muted"><a href="javascript:window.location.reload(true)">refresh</a> this page to see another game from the archive</p>
+    </div>
+    <?php foreach ($game->moves as $move) { ?>
+    <?php if ($move->player == 1) { ?>
+      <hr/>
+    <?php } ?>
+    <div class="move line-<?= $colours [$move->move]?>">
+      <div class="player">
+        <div class="circle"><?= $move->player ?></div>
+      </div>
+      <div class="play">
+        <div class="place"><?= $move->move ?></div>
+        <div class="note"><?= str_replace ('|', '&raquo;', $move->note) ?></div>
+      </div>
+    </div>
+    <?php } ?>
+    <hr/>
+    <p class="monospace">/* raw data feed */</p>
+    <pre class="raw"><?= json_encode ($game, JSON_PRETTY_PRINT | JSON_UNESCAPED_LINE_TERMINATORS) ?></pre>
+  </div>
+</body>
+</html>
